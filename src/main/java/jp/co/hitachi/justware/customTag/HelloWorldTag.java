17df9fd76168e768345b03d4b0a39a7486c84461
Tkouta <kouta1108taka_1994@outlook.jp>
2018-09-03 01:52:23 +0900

    カスタムタグのハンドラクラスとタグ設定ファイルを作成しました。

---
+++ src/main/java/jp/co/hitachi/justware/customTag/HelloWorldTag.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.co.hitachi.justware.customTag;
+
+import javax.servlet.jsp.PageContext;
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.tagext.Tag;
+import java.io.IOException;
+ 
+public class HelloWorldTag implements Tag{
+    private PageContext pageContext=null;
+    private Tag parentTag=null;
+
+    public void setPageContext(PageContext pageContext){
+        this.pageContext=pageContext;
+    }
+
+    public void setParent(Tag parentTag){
+        this.parentTag=parentTag;
+    }
+
+    public int doStartTag() throws JspException{
+        return SKIP_BODY;
+    }
+
+    public int doEndTag() throws JspException{
+        try{
+          pageContext.getOut().print("Hello World");
+        }catch(IOException e){
+          throw new JspException(e.getMessage());
+        }
+        return EVAL_PAGE;
+    }
+
+    public void release(){}
+
+    public Tag getParent(){
+        return parentTag;
+    }
+}
---
+++ src/main/webapp/customTag.jsp
@@ -0,0 +1,15 @@
+<%-- 
+    Document   : customTag
+    Created on : 2018/09/03, 1:39:33
+    Author     : 康太
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<%@ taglib uri="/WEB-INF/tlds/HelloWorldTag.tld" prefix="sample" %>
+<html>
+<head><title>Hello World</title></head>
+<body>
+<sample:HelloWorld></sample:HelloWorld>
+</body>
+</html>
